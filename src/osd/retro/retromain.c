6c0a9af813d8773393ef635646bd21e235afb62d
markwkidd <mark@stardart.net>
2018-03-05 11:30:34 -0500

    whitespace

--- src/osd/retro/retromain.c
+++ src/osd/retro/retromain.c
@@ -533,8 +533,7 @@ void retro_set_environment(retro_environment_t cb)
 	"Set contrast; default|+1%|+2%|+3%|+4%|+5%|+6%|+7%|+8%|+9%|+10%|+11%|+12%|+13%|+14%|+15%|+16%|+17%|+18%|+19%|+20%|-20%|-19%|-18%|-17%|-16%|-15%|-14%|-13%|-12%|-11%|-10%|-9%|-8%|-7%|-6%|-5%|-4%|-3%|-2%|-1%" },
       { "mame_current_adj_gamma",
 	"Set gamma; default|+1%|+2%|+3%|+4%|+5%|+6%|+7%|+8%|+9%|+10%|+11%|+12%|+13%|+14%|+15%|+16%|+17%|+18%|+19%|+20%|-20%|-19%|-18%|-17%|-16%|-15%|-14%|-13%|-12%|-11%|-10%|-9%|-8%|-7%|-6%|-5%|-4%|-3%|-2%|-1%" },
-      { "mame-external_hiscore", "Use external hiscore.dat; disabled|enabled" },      
-
+      { "mame-external_hiscore", "Use external hiscore.dat; disabled|enabled" },
       { NULL, NULL },
    };
 
