beb9e51cf9f0cbc6c12b9cf6decfc444d6b3db19
Kyland K <kkainbryce@oh.rr.com>
2021-03-07 12:53:09 -0500

    Additional FS Values (#147)
    Helps Lower Spec with stubborn Games, such as Crystal of Kings!

--- src/osd/retro/retromain.c
+++ src/osd/retro/retromain.c
@@ -533,7 +533,7 @@ void retro_set_environment(retro_environment_t cb)
       { "mame_current_aspect_ratio", "Core provided aspect ratio; DAR|PAR" },
       { "mame_current_turbo_button", "Enable autofire; disabled|button 1|button 2|R2 to button 1 mapping|R2 to button 2 mapping" },
       { "mame_current_turbo_delay", "Set autofire pulse speed; medium|slow|fast" },
-      { "mame_current_frame_skip", "Set frameskip; 0|1|2|3|4|automatic" },
+      { "mame_current_frame_skip", "Set frameskip; 0|1|2|3|4|5|6|7|8|9|10|automatic" },
       { "mame_current_sample_rate", "Set sample rate (Restart); 48000Hz|44100Hz|32000Hz|22050Hz" },
       { "mame_current_adj_brightness",
 	"Set brightness; default|+1%|+2%|+3%|+4%|+5%|+6%|+7%|+8%|+9%|+10%|+11%|+12%|+13%|+14%|+15%|+16%|+17%|+18%|+19%|+20%|-20%|-19%|-18%|-17%|-16%|-15%|-14%|-13%|-12%|-11%|-10%|-9%|-8%|-7%|-6%|-5%|-4%|-3%|-2%|-1%" },
